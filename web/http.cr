beea7b77c5469e1ce55ccfb5190da25345c3d8e3
Rutledge, Julian A <jarutledge@seic.com>
2019-01-10 16:39:47 -0500

    first working webserver with ecr

--- web/http.cr
+++ web/http.cr
@@ -1,8 +1,18 @@
 require "http/server"
+require "ecr"
+
+class Hello
+	def initialize(@time : String)
+	end
+
+	ECR.def_to_s "time.ecr"
+end
+
+tn = Time.now.to_s
 
 server = HTTP::Server.new do |context|
-	context.response.content_type = "text/plain"
-	context.response.print "Hello World! The time is #{Time.now}\n"
+	context.response.content_type = "text/html"
+	context.response.print Hello.new(tn).to_s
 end
 
 address = server.bind_tcp 8080
